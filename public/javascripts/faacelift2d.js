4852d0c01452d422a33e510d2eb44004abfafee7
Aidan Feldman <aidan.feldman@gmail.com>
2011-12-06 02:31:49 -0500

    render different photo in sequence based on mouse pos

--- public/javascripts/faacelift2d.js
+++ public/javascripts/faacelift2d.js
@@ -1,12 +1,14 @@
 Faacelift = {
   init: function(){
+    this.$window = $(window);
     this.canvasEl = document.getElementById('canvas');
     this.ctx = this.canvasEl.getContext('2d');
+    
+    this.$window.mousemove($.proxy(this.onMouseMove, this));
   },
 
-  drawImage: function(){
-    var photoData = this.photos[0],
-      img = new Image(),
+  drawImage: function(photoData){
+    var img = new Image(),
       tag = photoData.tags[0],
       scale = 0.8,
       faceLeft = tag.center.x - (tag.width / 2),
@@ -16,10 +18,17 @@ Faacelift = {
       this.ctx.drawImage(img, faceLeft, faceTop, tag.width, tag.height, this.canvasEl.width * (1 - scale) / 2, this.canvasEl.height * (1 - scale) / 2, this.canvasEl.width * scale, this.canvasEl.height * scale);
     }, this);
     img.src = '/proxy?src=' + photoData.url;
+  },
+  
+  onMouseMove: function(e){
+    var numPhotos = this.photos.length,
+      photoIndex = Math.floor(e.pageX / (this.$window.width() / numPhotos)),
+      photoData = this.photos[photoIndex];
+    
+    this.drawImage(photoData);
   }
 };
 
 $(function(){
   Faacelift.init();
-  Faacelift.drawImage();
 });
