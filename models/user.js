153ff8b05c1388b8a5a9ef04da6e4eb2dfeb964d
Aidan Feldman <aidan.feldman@gmail.com>
2011-12-07 01:31:46 -0500

    committing progress w/ Futures (broken)

--- models/user.js
+++ models/user.js
@@ -2,28 +2,38 @@ var mongoose = require('mongoose'),
   face = require('node-face'),
   PhotoSchema = require('./photo.js').PhotoSchema,
   _u = require('underscore'),
-  util = require('util');
+  util = require('util'),
+  Futures = require('futures');
+
+
+var filters = [];
+
+function init(){
+  var yawMax = 40,
+    step = 10;
+  
+  for (var rangeMin = -1 * yawMax; rangeMin < yawMax; rangeMin += step){
+    var rangeMax = rangeMin + step;
+    filters.push('yaw:' + rangeMin + '~' + rangeMax);
+  }
+}
 
 var UserSchema = new mongoose.Schema({
   // mongoose-auth adds fields as well
   photos: [PhotoSchema],
 });
 
-UserSchema.methods.fetchPhotos = function(options){
-  console.log('fetching photos for user fbid:' + this.fb.id);
-  
+UserSchema.methods.fetchPhoto = function(options){
   var params = _u.extend({
-    scope: this
-  }, options);
-  
-  face.facebook.get({
+    attributes: 'none',
+    limit: 1,
     uids: this.fb.id,
     user_auth: {
       fb_user: this.fb.id,
       fb_oauth_token: this.fb.accessToken
     },
     success: function(data){
-      console.log('photos retrieved for user fbid:' + this.fb.id);
+      console.log('photos retrieved for user fbid:' + this.fb.id + ' filter:' + options.filter);
       
       var self = this;
       data.photos.forEach(function(photo){
@@ -38,12 +48,46 @@ UserSchema.methods.fetchPhotos = function(options){
       });
     },
     error: function(err, response, body){
-      console.log('photos retrieval failed for user fbid:' + this.fb.id + ' - ' + util.inspect(body));
+      console.log('photos retrieval failed for user fbid:' + this.fb.id + ' filter:' + options.filter + ' - ' + util.inspect(body));
       if (params.error){
         params.error.apply(params.scope, arguments);
       }
     },
     scope: this
+  }, options);
+  
+  face.facebook.get(params);
+};
+
+UserSchema.methods.fetchPhotos = function(options){
+  console.log('fetching photos for user fbid:' + this.fb.id);
+  
+  var join = Futures.join(options.scope || this);
+  
+  filters.forEach(_u.bind(function(filter){
+    var future = Futures.future(),
+      params = {
+        filter: filter,
+        success: function(data){
+          future.fulfill(null, data);
+        },        
+        error: function(err, response, body){
+          future.fulfill(err, body);
+        }
+      };
+    
+    join.add(future);
+    this.fetchPhoto(params);
+  }, this));
+  
+  join.whenever(function(err, data){
+    if (err){
+      console.log('join error: ', err, data);
+    }
+  });
+  
+  join.when(function(){
+    options.success.call(this);
   });
 };
 
@@ -55,4 +99,5 @@ UserSchema.methods.photoDataAsPx = function(){
 };
 
 
+init();
 exports.UserSchema = UserSchema;
